17cc152dbebe8e6d23fd5912de0adf13fb82fdaf
Mateus Danielle <mateusdanielle@edu.fecap.br>
2019-01-04 19:53:40 -0200

    Listando todos

--- src/index.js
+++ src/index.js
@@ -6,13 +6,15 @@ import store from "~/store";
 
 import { View } from "react-native";
 
+import TodoList from "./TodoList";
+
 // import styles from './styles';
 
 export default class src extends Component {
     render() {
         return (
             <Provider store={store}>
-                <View />
+                <TodoList />
             </Provider>
         );
     }
