51873a23d5fb776c6ca3226509dce7aa5e4f3e36
Mateus Danielle <mateusdanielle@edu.fecap.br>
2019-01-06 18:53:48 -0200

    Configurando novo app

--- src/index.js
+++ src/index.js
@@ -6,15 +6,13 @@ import store from "~/store";
 
 import { View } from "react-native";
 
-import TodoList from "./TodoList";
-
 // import styles from './styles';
 
 export default class src extends Component {
     render() {
         return (
             <Provider store={store}>
-                <TodoList />
+                <View />
             </Provider>
         );
     }
